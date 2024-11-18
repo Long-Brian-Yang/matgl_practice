7bfbd56371fd158a064f0fd0dd3fbcda3dd064af
Long-Brian-Yang <longyang2000110@gmail.com>
2024-11-18 16:40:14 +0900

    updated

---
+++ 04_training_megnet_eform/training_megnet_eform.py
@@ -0,0 +1,132 @@
+from __future__ import annotations
+
+import os
+import shutil
+import warnings
+import zipfile
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+from dgl.data.utils import split_dataset
+from pymatgen.core import Structure
+from pytorch_lightning.loggers import CSVLogger
+from tqdm import tqdm
+from matgl.ext.pymatgen import Structure2Graph, get_element_list
+from matgl.graph.data import MGLDataset, MGLDataLoader, collate_fn
+from matgl.layers import BondExpansion
+from matgl.models import MEGNet
+from matgl.utils.io import RemoteFile
+from matgl.utils.training import ModelLightningModule
+
+# To suppress warnings for clearer output
+warnings.simplefilter("ignore")
+
+from dgl import batch as dgl_batch
+import torch
+
+# Dataset preparation
+def load_dataset() -> tuple[list[Structure], list[str], list[float]]:
+    """Raw data loading function.
+
+    Returns:
+        tuple[list[Structure], list[str], list[float]]: structures, mp_id, Eform_per_atom
+    """
+    if not os.path.exists("mp.2018.6.1.json"):
+        f = RemoteFile("https://figshare.com/ndownloader/files/15087992")
+        with zipfile.ZipFile(f.local_path) as zf:
+            zf.extractall(".")
+    data = pd.read_json("mp.2018.6.1.json")
+    structures = []
+    mp_ids = []
+
+    for mid, structure_str in tqdm(zip(data["material_id"], data["structure"])):
+        struct = Structure.from_str(structure_str, fmt="cif")
+        structures.append(struct)
+        mp_ids.append(mid)
+
+    return structures, mp_ids, data["formation_energy_per_atom"].tolist()
+
+
+structures, mp_ids, eform_per_atom = load_dataset()
+
+structures = structures[:100]
+eform_per_atom = eform_per_atom[:100]
+
+# get element types in the dataset
+elem_list = get_element_list(structures)
+# setup a graph converter
+converter = Structure2Graph(element_types=elem_list, cutoff=4.0)
+# convert the raw dataset into MEGNetDataset
+mp_dataset = MGLDataset(
+    structures=structures,
+    labels={"Eform": eform_per_atom},
+    converter=converter,
+)
+
+train_data, val_data, test_data = split_dataset(
+    mp_dataset,
+    frac_list=[0.8, 0.1, 0.1],
+    shuffle=True,
+    random_state=42,
+)
+train_loader, val_loader, test_loader = MGLDataLoader(
+    train_data=train_data,
+    val_data=val_data,
+    test_data=test_data,
+    collate_fn=collate_fn,
+    batch_size=2,
+    num_workers=0,
+)
+
+# Model setup
+# setup the embedding layer for node attributes
+node_embed = torch.nn.Embedding(len(elem_list), 16)
+# define the bond expansion
+bond_expansion = BondExpansion(rbf_type="Gaussian", initial=0.0, final=5.0, num_centers=100, width=0.5)
+
+# setup the architecture of MEGNet model
+model = MEGNet(
+    dim_node_embedding=16,
+    dim_edge_embedding=100,
+    dim_state_embedding=2,
+    nblocks=3,
+    hidden_layer_sizes_input=(64, 32),
+    hidden_layer_sizes_conv=(64, 64, 32),
+    nlayers_set2set=1,
+    niters_set2set=2,
+    hidden_layer_sizes_output=(32, 16),
+    is_classification=False,
+    activation_type="softplus2",
+    bond_expansion=bond_expansion,
+    cutoff=4.0,
+    gauss_width=0.5,
+)
+
+# setup the MEGNetTrainer
+lit_module = ModelLightningModule(model=model)
+
+
+# Training
+logger = CSVLogger("logs", name="MEGNet_training")
+trainer = pl.Trainer(max_epochs=20, accelerator="cpu", logger=logger)
+trainer.fit(model=lit_module, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+# Visualizing the convergence
+metrics = pd.read_csv("logs/MEGNet_training/version_0/metrics.csv")
+metrics["train_MAE"].dropna().plot()
+metrics["val_MAE"].dropna().plot()
+
+_ = plt.legend()
+
+# This code just performs cleanup for this notebook.
+
+for fn in ("dgl_graph.bin", "lattice.pt", "dgl_line_graph.bin", "state_attr.pt", "labels.json"):
+    try:
+        os.remove(fn)
+    except FileNotFoundError:
+        pass
+
+shutil.rmtree("logs")
+
